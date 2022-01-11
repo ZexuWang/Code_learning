352ee0a5db71f00db826ebfbb0d4d9095a748832
Zexu <jorsonwzx@icloud.com>
2022-01-11 09:33:50 +0800

    Day 13 of stl

---
+++ C++/stl/isValidBST.cpp
@@ -0,0 +1,63 @@
+/*98. 验证二叉搜索树
+给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+
+有效 二叉搜索树定义如下：
+
+节点的左子树只包含 小于 当前节点的数。
+节点的右子树只包含 大于 当前节点的数。
+所有左子树和右子树自身必须也是二叉搜索树。
+ 
+
+示例 1：
+
+
+输入：root = [2,1,3]
+输出：true
+示例 2：
+
+
+输入：root = [5,1,4,null,null,3,6]
+输出：false
+解释：根节点的值是 5 ，但是右子节点的值是 4 。
+ 
+
+提示：
+
+树中节点数目范围在[1, 104] 内
+-231 <= Node.val <= 231 - 1
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        if (!root) return true;
+        long long inorder = (long long) INT_MIN - 1;
+        stack<TreeNode*> stk;
+        while(!stk.empty() || root){
+            while(root) {
+                stk.push(root);
+                root = root->left;
+            }
+            root = stk.top();
+            stk.pop();
+            if (root->val <= inorder) {
+                return false;
+            }
+            inorder = root->val;
+            root = root->right;
+        }
+    return true;
+
+    }
+};
